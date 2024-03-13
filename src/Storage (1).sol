545f5db9ea47ee1350aa524cf6fc85196f0d8f82
elishevapro <133571947+elishevapro@users.noreply.github.com>
2024-03-13 15:23:29 +0200

    add 2 files day #4

---
+++ src/Storage (1).sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.6.6;
+/**
+*@title Storage
+*@dev Store & retrieve value in a variable
+*/
+contract Storage{
+    uint256 number;
+
+    /**
+    *@dev Store value in a variable
+    *@param num value to store
+    */
+    function store(uint256 num) public{
+        number = num;
+    }
+
+    /**
+    *@dev Return value
+    *@return the 'number' value
+    */
+    function retrieve() public view returns(uint256){
+        return number;
+    }
+}
+
