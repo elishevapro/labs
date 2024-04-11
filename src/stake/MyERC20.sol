be711a113a20553b21684aebbeb134fe5ef6a912
elishevapro <elishevapro@gmail.com>
2024-04-11 16:55:33 +0300

    staking

--- src/stake/MyERC20.sol
+++ src/stake/MyERC20.sol
@@ -1,45 +1,16 @@
-//SPDX-License-Idntifier: MIT
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
-import "@openzeppelin/ERC20/ERC20.sol";
+
+import "@openzeppelin/ERC20/IERC20.sol";
 import "@openzeppelin/ERC20/extensions/ERC20Permit.sol";
-contract MyERC20 is ERC20, ERC20Permit {
-    address public owner;
-    uint public totalReward;
-    uint public totalStaked;
-    mapping(address=>uint) public stakedAmounts;
-    mapping(address=>uint) public beginDates;
-    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken")  {
-        totalReward = 1000000000;
-        owner = msg.sender;
-        _mint(address(this), totalReward);
+
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MTK") {
+     
     }
-    receive() external payable {
-        require(msg.sender.balance>=msg.value, "not enough money");
-    }
-    function send(address to, uint amount) external {
-        _mint(to, amount);
-    }
-    function stake() external payable{
-        require(msg.value>0, "must be positive amount");
-        stakedAmounts[msg.sender] += msg.value;
-        beginDates[msg.sender] = block.timestamp;
-        //console.log(block.timestamp);
-        totalStaked+=msg.value;
-        payable(address(this)).transfer(msg.value);
-    }
-    function withdraw() external {
-        require(stakedAmounts[msg.sender]>0,"you didn't stake anything");
-        uint beginDate = beginDates[msg.sender];
-        uint dayPassed = (block.timestamp-beginDate)/60/60/24;
-        require(dayPassed >= 7, string(abi.encodePacked("Your money is still locked.")));
-        uint reward = totalReward * ( stakedAmounts[msg.sender]/totalStaked);
-        _mint(address(this), reward);
-        payable(msg.sender).transfer(stakedAmounts[msg.sender]+reward);
-    }
-    function getBalance() public returns (uint) {
-        return address(this).balance;
-    }
-    function stakedAmount() public returns (uint) {
-        return stakedAmounts[msg.sender];
+
+    function mint(address add, uint amount) public {
+        _mint(add , amount);
     }
 }
--- src/stake/stake.sol
+++ src/stake/stake.sol
@@ -1,49 +1,64 @@
-//SPDX-License-Identifier: MIT
+ //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
+//import "../../lib/openzeppelin-contracts/contracts/token/ERC20"
 import "forge-std/console.sol";
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../../lib/solmate/src/tokens/ERC20.sol";
-
-contract Stake is ERC20 {
+import "@openzeppelin/ERC20/IERC20.sol";
+import "./MyERC20.sol";
+//import "../../lib/solmate/src/tokens/ERC20.sol";
+contract Stake{
     address public owner;
-    // ERC20 public immutable erc20;
+    MyToken public immutable token;
     uint public totalReward;
     uint public totalStaked;
+    uint WAD =10**18;
     mapping(address=>uint) public stakedAmounts;
     mapping(address=>uint) public beginDates;
-    constructor() ERC20('COIN', 'CN',18) {
+    constructor(address _token) {
+        token = MyToken(_token);
         totalReward = 1000000;
         owner = msg.sender;
-        // erc20 = new ERC20('COIN', 'CN',18);
-        // erc20.
-        _mint(address(this), totalReward);
-        // address(this).balance+=1000;??????????
+        token.mint(address(this), totalReward);
+        console.log("this" ,token.balanceOf(address(this)));
     }
+
+    function mint(address to ,uint amount) public{
+        token.mint(to, amount);
+    }
+
     receive() external payable {
-        require(msg.sender.balance>=msg.value, "not enough money");
+    //    require(balanceOf(person1)>=msg.value, "not enough money");
     }
     function stake() external payable{
         require(msg.value>0, "must be positive amount");
+        console.log(token.balanceOf(msg.sender));
+        console.log("msg.value",msg.value);
+        require(token.balanceOf(msg.sender) >= msg.value  ,"no money");
         stakedAmounts[msg.sender] += msg.value;
         beginDates[msg.sender] = block.timestamp;
-        console.log(block.timestamp);
         totalStaked+=msg.value;
-        payable(address(this)).transfer(msg.value);
+        token.transferFrom(msg.sender,address(this),msg.value);
+        //payable(address(this)).transfer(msg.value);
+        console.log(token.balanceOf(msg.sender));
     }
     function withdraw() external {
         require(stakedAmounts[msg.sender]>0,"you didn't stake anything");
         uint beginDate = beginDates[msg.sender];
         uint dayPassed = (block.timestamp-beginDate)/60/60/24;
-        require(dayPassed >= 7, string(abi.encodePacked("Your money is still locked.")));
-        uint reward = totalReward * ( stakedAmounts[msg.sender]/totalStaked);
-        _mint(address(this), reward);
-        payable(msg.sender).transfer(stakedAmounts[msg.sender]+reward);
+        require(dayPassed >= 7, "Your money is still locked.");
+        uint reward = totalReward * ( stakedAmounts[msg.sender]*WAD/totalStaked)/WAD;
+        
+        console.log(reward);
+         console.log("this" ,token.balanceOf(address(this)));
+        token.mint(address(this), reward);
+        // _approve(address(this), msg.sender,stakedAmounts[msg.sender]+reward);
+        token.transfer( msg.sender,stakedAmounts[msg.sender]+reward);
+        delete stakedAmounts[msg.sender];
+        delete beginDates[msg.sender];        
     }
     function getBalance() public returns (uint) {
-        return address(this).balance;
+        return token.balanceOf(address(this));
     }
     function stakedAmount() public returns (uint) {
         return stakedAmounts[msg.sender];
