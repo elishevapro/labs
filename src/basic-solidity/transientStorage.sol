ce6da8989c721a0ec16ad3a5c18e81d7a4619328
elishevapro <elishevapro@gmail.com>
2024-06-16 16:23:57 +0300

    basic solidity 16.6.24

--- src/basic-solidity/transientStorage.sol
+++ src/basic-solidity/transientStorage.sol
@@ -1,2 +1,32 @@
 pragma solidity ^0.8.19;
 
+interface Itest {
+    function val() external returns (uint);
+    function test() external;
+}
+
+contract Fallback {
+    uint val;
+    fallback() external {
+        val = Itest(msg.sender).val();
+    }
+    function test(address target) external {
+        Itest(target).test();
+    }
+}
+contract TestStorage {
+    uint val;
+    function test() public {
+        val = 123;
+        bytes memory b = "";
+        msg.sender.call(b);
+    }
+}
+contract TestTransientStorage {
+    bytes32 constant SLOT = 0;
+    function test() external {
+        assembly {
+            tstore
+        }
+    }
+}
